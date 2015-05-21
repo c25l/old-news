7120e4254247ded37faa0ed420f55015addd1646
EC2 Default User <ec2-user@ip-172-31-19-157.us-west-2.compute.internal>
2015-05-21 14:32:11 +0000

    updates and twitter support

--- feed_digest.py
+++ feed_digest.py
@@ -12,6 +12,8 @@ import urllib
 import json
 import twitter
 import sys
+import datetime
+import dateutil.parser
 
 def parse_item_or_list(either):
     if 'xmlUrl' in either.attrib:
@@ -41,6 +43,9 @@ def feed_info(feed):
 def recent(feed):
     return time.mktime(time.localtime()) - time.mktime(feedtime(feed)) < 60*60*26
 
+def recent_tweet(tweet):
+    tdiff = time.mktime(datetime.datetime.now().timetuple()) - time.mktime(dateutil.parser.parse(tweet['created_at']).timetuple())
+    return tdiff < 60*60*26
 
 def feedtime (feed):
     if 'published' in feed:
@@ -53,18 +58,12 @@ def feedtime (feed):
         return False
     
     
-def parse_feeds(opml_loc):
+def parse_feeds(feed_uris):
     try:
-        opml = []
-        for feed in etree.parse(opml_loc).getroot()[1]:
-            try:
-                opml.extend(parse_item_or_list(feed))
-            except:
-                pass
-        feeds = [ feedparser.parse(feed) for feed in opml]
+        feeds = [ feedparser.parse(feed) for feed in feed_uris]
         return [{'title': feed_title(feed), 'entries': recent_items_for_feed(feed.entries)} for feed in feeds] 
     except:
-        print('failure')
+        print('feed parse failure')
         return []
     
     
@@ -82,7 +81,7 @@ def send_email(title, body, setup):
     try:
         msg = MIMEMultipart('alternative')
         msg['Subject'] = title 
-        msg['From'] = setup['email_dest']
+        msg['From'] = "\"Old News\" <" +setup['email_dest']+">"
         msg['To'] = setup['email_dest']
         html = str(body)
         part2 = MIMEText(html, 'html')
@@ -91,10 +90,10 @@ def send_email(title, body, setup):
         s.login(setup['email_dest'], setup['email_pass'])
         s.sendmail(setup['email_dest'], setup['email_dest'], msg.as_string())
         s.quit()
-        return 1
+        return True
     except:
         print('email failure? ', sys.exc_info())
-        return 0
+        return False
 
     
 def feeds_to_html(feeds):
@@ -103,18 +102,42 @@ def feeds_to_html(feeds):
         if x['entries']:
             outstr+="<h3>"+x['title']+"</h3><br>"
             for y in x['entries']:
-                outstr+="<a href=" + y['link'] +">"+y['title']+"</a><br>" + y['summary']+ "<br><br>" 
+                outstr+="<a href=" + y['link'] +">"+y['title']+"</a><br>" + y['summary'][:500]+ "<br><br>\n" 
             outstr+="<hr>"
     return outstr+"</body>"
+
+
+def twitter_to_html(tweets):
+    outstr="<body> Twitter: <br>\n"
+    for x in tweets:
+        outstr+= "<h4><img src=\""+x['image']+"\"> @"+x['screen_name']+ "  " + x['user'] + "<br></h4>\n"
+        outstr+= x['text'] + " <br><br>"
+    return outstr + "</body>\n"
+
+def get_twitter(setup):
+    try:
+        twit = twitter.Twitter(auth=twitter.OAuth(setup['twitter_access_token'],
+                                                  setup['twitter_access_secret'],
+                                                  setup['twitter_consumer_key'],
+                                                  setup['twitter_consumer_secret']))
+        return [{"time": x['created_at'], 
+                 "text": x['text'], 
+                 "user": x['user']['name'], 
+                 "image": x['user']['profile_image_url'], 
+                 "screen_name": x['user']['screen_name']}  
+                for x in twit.statuses.home_timeline(count=40) if recent_tweet(x)]
+    except:
+        print("twitter error", sys.exc_info())
+        return []
 
 
 def main():
     setup=json.load(open('feed_digest.json','r'))
     send_email(get_weather(setup['weather_api_key'],
                            setup['weather_location']), 
-               feeds_to_html(parse_feeds(setup['opml_location'])),
+               feeds_to_html(parse_feeds(setup['feeds'])) +
+               twitter_to_html(get_twitter(setup)),
                setup)
-    
     
 if __name__ == "__main__":
     main()
