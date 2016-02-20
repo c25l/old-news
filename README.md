6b59025691d2f107936af1c000ba36e17b8342dd
Chris Bonnell <cpbnel@gmail.com>
2016-02-20 16:36:01 -0600

    push to gitlab

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Old_news
+# Rss_Digest
 
 This is a python script that is designed to take some email details, and an opml file. 
 You run it, and it gives you a day's worth of feed activity. It would be best on a crontab someplace.
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cd /home/chris/Dropbox/old_news
-nohup /home/chris/miniconda2/bin/python old_news.py authinfo.json config.json bloom.bl > errors.out &
+cd /home/chris/Dropbox/Rss_Digest/
+nohup /home/chris/miniconda2/bin/python rss_digest.py authinfo.json config.json bloom.bl > errors.out &
